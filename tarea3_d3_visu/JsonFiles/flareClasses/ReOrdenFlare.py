ac5504466e341480a9f4541430eaef699c46d99e
samcab28 <samircabrera2528@gmail.com>
2024-05-06 23:08:03 -0600

    clase flare y titulos html

--- tarea3_d3_visu/JsonFiles/flareClasses/ReOrdenFlare.py
+++ tarea3_d3_visu/JsonFiles/flareClasses/ReOrdenFlare.py
@@ -4,16 +4,17 @@ import pandas as pd
 df = pd.read_csv('C:/Users/samir/Visualizacion/tarea3_d3_visu/JsonFiles/flareClasses/flareClasses.csv')
 
 # Filtra los registros que contienen los puntos necesarios en la columna 'id'
-filtered_df = df[df['id'].str.count('\\.') >= 2]
+filtered_df = df[df['id'].str.count('\\.') >= 3].copy()  # Copia el DataFrame para evitar la advertencia
 
-# Divide la columna 'id' en 'package', 'function' y 'name'
+# Divide la columna 'id' en 'package', 'function', 'name' y 'secondName'
 split_data = filtered_df['id'].str.split('\\.', expand=True)
-filtered_df['package'] = split_data[0]
-filtered_df['function'] = split_data[1]
-filtered_df['name'] = split_data[2]
+filtered_df.loc[:, 'package'] = split_data[0]
+filtered_df.loc[:, 'function'] = split_data[1]
+filtered_df.loc[:, 'name'] = split_data[2]
+filtered_df.loc[:, 'secondName'] = split_data[3]
 
 # Reorganiza las columnas
-filtered_df = filtered_df[['id', 'package', 'function', 'name', 'value']]
+filtered_df = filtered_df[['id', 'package', 'function', 'name', 'secondName', 'value']]
 
 # Guarda el nuevo CSV
-filtered_df.to_csv('flareClassesOrdenado.csv', index=False)
+filtered_df.to_csv('flareClassesOrdenadoRadial.csv', index=False)
