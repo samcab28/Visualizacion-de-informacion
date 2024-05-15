5ccf6c59830f3704e00d7e259892a6db49027c75
samcab28 <samircabrera2528@gmail.com>
2024-05-15 17:59:33 -0600

    update data

--- Proyecto2/data/distritosCr/OrdenarFormato.py
+++ Proyecto2/data/distritosCr/OrdenarFormato.py
@@ -1,10 +1,7 @@
 import pandas as pd
 
-
-
 # Lee el archivo CSV original
 df = pd.read_csv('C:/Users/samir/Visualizacion/Proyecto2/data/distritosCr/distritosCr.csv')
-
 
 # Divide la columna ID en provincia, canton y distrito
 df[['PROVINCIA', 'CANTON', 'DISTRITO']] = df['ID'].str.split('.', expand=True)
@@ -14,3 +11,11 @@ df = df[['ID', 'PROVINCIA', 'CANTON', 'DISTRITO', 'COD_POSTAL', 'AREA_KM2', 'POB
 
 # Guarda el nuevo CSV
 df.to_csv('./Proyecto2/data/distritosCr/DistritosCrOrdenado.csv', index=False)
+
+# Selecciona solo las columnas deseadas
+#se debe de tener en cuenta que ademas de la poblacion del agno 2022, se puede tener en base 
+#al area o a otros agnos
+df_selected = df[['PROVINCIA', 'CANTON', 'DISTRITO', 'POBL_2022']]
+
+# Guarda el DataFrame seleccionado en formato csv
+df_selected.to_csv('./Proyecto2/data/distritosCr/DistritosCrSeleccionado.csv', index=False)
